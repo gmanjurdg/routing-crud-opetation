cbcc7cd5b338441adee525e7419e10b943049849
gmanjurdg <53145356+gmanjurdg@users.noreply.github.com>
2019-08-26 23:46:52 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,62 @@
+const books = {
+    AbdulKalam:{  
+        name:'Abdul Kalam',
+        likes: '123.0k',
+        banner:'http://sakshieducation.com/ S qvtsxmrommcw1ym3yktwkzfv  /Images/Books/My_Journey.jpg'
+       },
+       MyHiddenChimp :{  
+        name:'My Hidden Chimp',
+        likes:'1m',
+        banner:'https://images-na.ssl-images-amazon.com/images/I/51DDGWXozRL._SX352_BO1,204,203,200_.jpg'
+       },
+
+       MyJourney: {  
+        name:'My Journey',
+        likes: '189.0k',
+        banner:'https://images2.penguinrandomhouse.com/cover/9781524763770'
+       }
+    }
+
+const movie ={
+    Maharshi:{  
+        name:'Maharshi',
+        likes: '123.0k',
+        banner:'https://images.news18.com/ibnlive/uploads/2019/04/MaharShi-Mahesh-Babu.jpg'
+       },
+       Avengers:{  
+        name:'Avengers:Endgame',
+        likes:'1m',
+        banner:'https://img.timesnownews.com/story/1546625126-Avengers_Imax_Poster.jpg?d=600x450'
+       },
+
+       Jersey:{  
+        name:'Jersey',
+        likes:'189.0k',
+        banner:'https://s3.ap-south-1.amazonaws.com/hansindia-bucket/h-upload/feeds/2019/04/20/166274-jersy-2.jpg'
+       }
+    }
+
+const mobile = {
+    Iphone:{  
+        name: 'Iphone',
+        likes:'123.0k',
+        banner:'https://cdn.tmobile.com/content/dam/t-mobile/en-p/cell-phones/apple/apple-iphone-xr/blue/Apple-iPhoneXr-Blue-2-3x.jpg'
+       },
+     Redmi5: {  
+        name:'Redmi Nonte-5 pro',
+        likes:'1m',
+        banner:'https://www.91-img.com/pictures/125125-v3-xiaomi-redmi-note-5-pro-6gb-ram-mobile-phone-large-1.jpg'
+       },
+
+       Redmi6:{  
+        name: 'Redmi Nonte-6 pro',
+        likes:'189.0k',
+        banner:'https://images-na.ssl-images-amazon.com/images/I/41Xg1-BI%2B0L.jpg'
+       },
+       Redmi7:{  
+        name:'Redmi Nonte-7 pro',
+        likes:'189.0k',
+        banner:'https://rounded.com/images/detailed/120/xiaomi-redmi-note-5-redmi-note-5-pro-battery-cover-blue.png'
+       }
+    }
+    export default {mobile, books, movie};
